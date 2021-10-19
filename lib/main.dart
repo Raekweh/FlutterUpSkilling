5f25a56c8ecfb320607ab0c7c5c2de8d85f46674
Raekweh <Raekweh009@gmail.com>
2021-10-19 14:38:39 +1300

    Google maps

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
-import 'dart:js_util';
-
 import 'package:flutter/material.dart';
-import 'package:upskillingproject/src/app.dart';
+import 'package:upskillingproject/src/search.dart';
 
-void main() {
-  loginMenu login = loginMenu();
-  runApp(login);
+// void main() {
+//   loginMenu login = loginMenu();
+//   runApp(login);
+// }
+
+void main() => runApp(myApp());
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'Parking App',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: Search(),
+    );
+  }
 }
---
+++ lib/src/search.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+class Search extends StatelessWidget{
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      body: Column(children: <Widget>[
+        Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: GoogleMap(
+            initialCameraPosition: CameraPosition(target: LatLng(-36.848461,174.763336),
+            zoom:  9.0),
+            zoomGesturesEnabled: true,
+          ),
+        )
+      ],),
+    );
+  }
+}
