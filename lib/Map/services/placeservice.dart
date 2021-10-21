78cda68cd07bdb08b27b2f1bf094f77d790803e3
Raekweh <Raekweh009@gmail.com>
2021-10-21 18:23:30 +1300

    Adding geo loc place models

---
+++ lib/Map/services/placeservice.dart
@@ -0,0 +1,15 @@
+import 'package:upskillingproject/Map/models/place.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+class PlaceService {
+  final key = 'AIzaSyDUbtDRWEoLx2NeEllbBg2Q24OXB8e7C1g';
+
+  Future<List<Place>> getPlaces(double lat, double lng) async {
+    var response = await http.get(Uri.parse(
+        'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$lng&type=parking&key=AIzaSyDUbtDRWEoLx2NeEllbBg2Q24OXB8e7C1g&radius=1500'));
+    var json = convert.jsonDecode(response.body);
+    var jsonResults = json['results'] as List;
+    return jsonResults.map((place) => Place.fromJson(place)).toList();
+  }
+}
