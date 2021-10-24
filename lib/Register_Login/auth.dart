a745857d5a3500eff3e85c844a3ae0ecccbfc8ba
Raekweh <Raekweh009@gmail.com>
2021-10-25 01:01:19 +1300

    Creating App users uid

--- lib/Register_Login/auth.dart
+++ lib/Register_Login/auth.dart
@@ -1,14 +1,21 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:ParkingApp/Register_Login/models/AppUserInfo.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //create user object based on firebaseuser
+  AppUserInfo? _userFromFirebaseUser(User user)
+  {
+    return user != null ? AppUserInfo(uid: user.uid) : null;
+  }
 
   //Sign in anon
   Future signInAnon() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
-      return user;
+      return _userFromFirebaseUser(user!);
     } catch (e) {
       print(e.toString());
       return null;
---
+++ lib/Register_Login/models/AppUserInfo.dart
@@ -0,0 +1,6 @@
+class AppUserInfo
+{
+  final String uid;
+
+  AppUserInfo({required this.uid});
+}
