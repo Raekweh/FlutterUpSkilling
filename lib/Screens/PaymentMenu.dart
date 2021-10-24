c69522bb0a5b0af932bbd8576d1f993dd937bdc9
Raekweh <Raekweh009@gmail.com>
2021-10-24 20:12:30 +1300

    Complete

--- lib/Screens/PaymentMenu.dart
+++ lib/Screens/PaymentMenu.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:upskillingproject/Payment/SelectingHoursMenu.dart';
+import 'package:upskillingproject/Payment/Validation.dart';
 
 class PaymentMenu extends StatefulWidget {
   const PaymentMenu({Key? key}) : super(key: key);
@@ -11,67 +13,98 @@ class PaymentMenu extends StatefulWidget {
 class _PaymentMenuState extends State<PaymentMenu> {
   TextEditingController areaIDController = TextEditingController();
   TextEditingController vechileIDController = TextEditingController();
+  Validation _validation = new Validation();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
-        appBar: AppBar(
-          title: Text('Payment'),
-        ),
-        body: Center(
-          child: new Column(children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(10),
-              child: Text(
-                'Area ID',
-                style: TextStyle(fontSize: 15, color: Colors.black),
-              ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Payment'),
             ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                controller: areaIDController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Area ID',
+            body: Center(
+                child: Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'Area ID',
+                    style: TextStyle(fontSize: 15, color: Colors.black),
+                  ),
                 ),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: Text(
-                'Vechile ID',
-                style: TextStyle(fontSize: 15, color: Colors.black),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(10),
-              child: TextField(
-                controller: vechileIDController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Vechile ID',
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: areaIDController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Area ID',
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.all(25),
-              child: FlatButton(
-                child: Text(
-                  'Next',
-                  style: TextStyle(fontSize: 20.0),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'Vechile ID',
+                    style: TextStyle(fontSize: 15, color: Colors.black),
+                  ),
                 ),
-                color: Colors.blueAccent,
-                textColor: Colors.white,
-                onPressed: () {
-
-                },
-              ),
-            ),
-          ],)
-        )
-      )
-    );
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: vechileIDController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Vechile ID',
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(25),
+                  child: FlatButton(
+                    child: Text(
+                      'Next',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                    color: Colors.blueAccent,
+                    textColor: Colors.white,
+                    onPressed: () {
+                      if (_validation.validVehiclePlate(vechileIDController.text) && _validation.validAreaID(areaIDController.text)) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SelectingHours()));
+                      }
+                      else
+                        {
+                          showDialog( context: context, builder: (BuildContext context) => _buildPopupDialog(context));
+                        }
+                    },
+                  ),
+                ),
+              ],
+            ))));
   }
+}
+
+Widget _buildPopupDialog(BuildContext context) {
+  return new AlertDialog(
+    title: const Text('Error'),
+    content: new Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text("Please check your inputs"),
+      ],
+    ),
+    actions: <Widget>[
+      new FlatButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        textColor: Theme.of(context).primaryColor,
+        child: Text('Close'),
+      ),
+    ],
+  );
 }
